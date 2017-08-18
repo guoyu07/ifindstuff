439f57e83d292d98cc37e4803c6b2c78919930b7
Elhachimi Med <Med@localhost.md>
2017-08-18 01:46:10 +0100

    first commit

---
+++ src/main/java/com/ifindstuff/repository/RoleRepository.java
@@ -0,0 +1,17 @@
+package com.ifindstuff.repository;
+
+
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.ifindstuff.model.Role;
+
+
+
+
+@Repository("roleRepository")
+public interface RoleRepository extends JpaRepository<Role, Integer>{
+	Role findByRole(String role);
+
+}
